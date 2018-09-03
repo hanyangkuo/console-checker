7ec8c53f00a101adc3ea3118f05c3488f69c2ba7
Han-Yang Kuo <tw01472760@gmail.com>
2018-09-03 20:05:15 +0800

    Update console_check.cpp

--- console_check.cpp
+++ console_check.cpp
@@ -1,43 +1,52 @@
 #include <conio.h>
 #include <iostream>
+#include <Windows.h>
+#include <string>
 
+#define LEFT 75
+#define RIGHT 77
 using namespace std;
 
+unsigned int color_y = 0 | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
+unsigned int color_n = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
 
 // show the question and allow user to use arrow key to choose yes or no and return the bool
 bool check(const string &qes) {
-	unsigned int color_y = 0 | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
-	unsigned int color_n = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
-	bool flag = true;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_n);
 	cout << qes;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_y);
 	cout << "Yes";
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_n);
 	cout << "/No";
-	getchar();
-	while (1) {
-		if (GetKeyState(VK_LEFT)<0 && kbhit()) {
-			flag = true;
-			cout << "\r" << qes;
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_y);
-			cout << "Yes";
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_n);
-			cout << "/No";
-		}
-		else if (GetKeyState(VK_RIGHT)<0 && kbhit()) {
-			flag = false;
-			cout << "\r" << qes;
-			cout << "Yes/";
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_y);
-			cout << "No";
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_n);
-		}
-		else if (GetKeyState(VK_RETURN) < 0 && kbhit()) {
-			return flag;
-		}
-		Sleep(33);
-		//GetAsyncKeyState  / GetKeyState
-	}
+	bool flag = true;
 
+	while (1) {
+		if (_kbhit()) {
+			int key_fist = _getch();
+			if (key_fist == 224) {
+				int key_second = _getch();
+				if (key_second == LEFT) {
+					cout << "\r" << qes;
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_y);
+					cout << "Yes";
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_n);
+					cout << "/No";
+					flag = 1;
+				}
+				else if (key_second == RIGHT) {
+					cout << "\r" << qes;
+					cout << "Yes/";
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_y);
+					cout << "No";
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_n);
+					flag = 0;
+				}
+			}
+			else if (key_fist == 13) {
+				cout << endl;
+				return (flag);
+			}
+			Sleep(33);
+		}
+	}
 }
